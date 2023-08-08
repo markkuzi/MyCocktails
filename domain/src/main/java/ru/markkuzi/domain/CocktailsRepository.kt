70f1b4fb523dbe43ae55d96b8fd7a9f933b18daa
Markel Kuznetsov <markkuzi@yandex.ru>
2023-08-08 13:45:06 +0300

    add get cocktail details use case and fun in repository

--- domain/src/main/java/ru/markkuzi/domain/CocktailsRepository.kt
+++ domain/src/main/java/ru/markkuzi/domain/CocktailsRepository.kt
@@ -6,6 +6,8 @@ interface CocktailsRepository {
 
     fun getCocktails(): List<Cocktail>
 
+    fun getCocktailDetails(cocktailId: Int): Cocktail
+
     fun createNewCocktail(cocktail: Cocktail)
 
     fun editCocktail(cocktail: Cocktail)

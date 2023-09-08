7ee99077beb07d50bae2ca801ebffc79c60c3c99
Markel Kuznetsov <markkuzi@yandex.ru>
2023-09-09 00:22:41 +0300

    Add edge-to-edge format

--- app/src/main/java/ru/markkuzi/mycocktails/presentation/MainActivity.kt
+++ app/src/main/java/ru/markkuzi/mycocktails/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.markkuzi.mycocktails.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import ru.markkuzi.mycocktails.R
 
@@ -10,5 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 }
